83b8f4f3d4910ff4988c423c9cbeb9c9c452b42b
joy-wyckoff <52981189+joy-wyckoff@users.noreply.github.com>
2019-07-31 15:57:04 -0700

    Add files via upload

---
+++ Private_Messaging_System/login.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<style>
+*{margin:0px; padding:0px;}
+#container{ width:300px; margin: 0px auto;}
+.input {width: 92%;padding:2%;}
+</style>
+<body>
+  <h1 align="center">Login Form</h1>
+  <div id="container">
+    <form method="post">
+      <input type="text" class="input" placeholder="user_name" name="user_name" /><br><br>
+      <input type="text" class="input" placeholder="user_name" name="user_name" /><br><br>
+    </form>
+  </div>
+</body>
+</html>
